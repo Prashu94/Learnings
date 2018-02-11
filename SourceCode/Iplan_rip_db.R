d70caf27fd5df701a9d674a7076bc86e0cf4af73
Prashant <prashantbhat94@gmail.com>
2018-02-11 19:53:49 +0530

    Add files via upload

--- SourceCode/Iplan_rip_db.R
+++ SourceCode/Iplan_rip_db.R
@@ -11,11 +11,15 @@ str(idf)
 idf$PLANNER_MONTH_YEAR<- format(idf$PLANNER_MONTH_YEAR,"%d-%b-%Y")
 idf$START_DATE<- format(idf$START_DATE,"%d-%b-%Y")
 idf$END_DATE<- format(idf$END_DATE,"%d-%b-%Y")
-dbWriteTable(con,"RIP_DB",idf)
-#query1<-"UPDATE rip_db SET PID='%s',SCP_ID='%s',SUB_ACTIVITY_NAME='%s',COMPLETION_PERCENT= %s,ACTUAL_EFFORT= %s,END_DATE= '%s',REMARKS= '%s',CLIENT= '%s',ENV= '%s',RCA= '%s'"
 
+query1<-paste0("UPDATE rip_db SET PID='%s',SCP_ID='%s',SUB_ACTIVITY_NAME='%s',COMPLETION_PERCENT= %s,ACTUAL_EFFORT= %s,END_DATE= '%s',REMARKS= '%s',CLIENT= '%s',ENV= '%s',RCA= '%s'
+          WHERE COMPLETION_PERCENT IS NULL AND END_DATE IS NULL")
+query2<-"commit"
+idf$PID[1]
 for(row in 1:nrow(idf)) {
-  dbSendUpdate(conn = con,"UPDATE rip_db SET PID = ",idf$PID[row],",  OWNER ='",idf$OWNER[row],",SCP_ID=",idf$SCP_ID[row],",SUB_ACTIVITY_NAME='",idf$SUB_ACTIVITY_NAME[row],"',COMPLETION_PERCENT=",idf$COMPLETION_PERCENT[row],",ACTUAL_EFFORT=",idf$ACTUAL_EFFORT[row],"END_DATE=",idf$END_DATE[row],",REMARKS=",idf$REMARKS[row],",CLIENT=",idf$CLIENT[row],",ENV=",idf$ENV[row],",RCA=",idf$RCA[row],"")
-  dbCommit(conn = con)
+  
+  
+  dbSendUpdate(conn = con,paste(sprintf(query1,idf$PID[row],idf$SCP_ID[row],idf$SUB_ACTIVITY_NAME[row],idf$COMPLETION_PERCENT[row],idf$ACTUAL_EFFORT[row],idf$END_DATE[row],idf$REMARKS[row],idf$CLIENT[row],idf$ENV[row],idf$RCA[row],query2),collapse=""))
+  
   }
 #dbSendUpdate(con,paste(sprintf(query1,idf$PID[row],idf$SCP_ID[row],idf$SUB_ACTIVITY_NAME[row],idf$COMPLETION_PERCENT[row],idf$ACTUAL_EFFORT[row],idf$END_DATE[row],idf$REMARKS[row],idf$CLIENT[row],idf$ENV[row],idf$RCA[row],query2),collapse=""))
